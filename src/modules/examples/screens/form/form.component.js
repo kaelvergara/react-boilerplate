65a6e543447a77ff178ad7231bc6af886cd9b839
Nikael Vergara <nvergara@exist.com>
2020-07-21 16:15:36 +0800

    Refactor Form component

--- src/modules/examples/screens/form/form.component.js
+++ src/modules/examples/screens/form/form.component.js
@@ -6,29 +6,12 @@ import { DevTool } from 'react-hook-form-devtools'
 import ErrorText from 'shared/components/form/errorText/errorText.component'
 import 'react-datepicker/dist/react-datepicker.css'
 
-const ErrorField = ({ error, messages }) => {
-  if (!error) {
-    return null
-  }
-
-  const errorMessages = {
-    required: 'This field is required',
-    maxLength: 'Max length reached',
-    minLength: 'Min length required',
-    pattern: 'Invalid pattern',
-    ...messages,
-  }
-
-  return <ErrorText>{errorMessages[error.type] || 'Error'}</ErrorText>
-}
-
 const Form = (props) => {
   const { register, handleSubmit, control, errors } = useForm()
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'skills',
   })
-
   const onSubmit = React.useCallback((values) => console.log({ values }), [])
 
   return (
@@ -36,14 +19,9 @@ const Form = (props) => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>First Name</label>
-          <input
-            type="text"
-            placeholder="First name"
-            name="firstName"
-            ref={register({ required: true, maxLength: 80 })}
-          />
-          <ErrorField
-            error={errors['firstName']}
+          <input type="text" placeholder="First name" name="firstName"
+            ref={register({ required: true, maxLength: 80 })}/>
+          <ErrorText error={errors['firstName']}
             messages={{
               required: 'First name is required',
               maxLength: 'Must not exceed 80 characters',
@@ -52,14 +30,10 @@ const Form = (props) => {
         </div>
         <div>
           <label>Last Name</label>
-          <input
-            type="text"
-            placeholder="Last name"
-            name="lastName"
+          <input type="text" placeholder="Last name" name="lastName"
             ref={register({ required: true, maxLength: 100 })}
           />
-          <ErrorField
-            error={errors['lastName']}
+          <ErrorText error={errors['lastName']}
             messages={{
               required: 'Last name is required',
               maxLength: 'Must not exceed 100 characters',
@@ -68,14 +42,10 @@ const Form = (props) => {
         </div>
         <div>
           <label>Email</label>
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
+          <input type="text" placeholder="Email" name="email"
             ref={register({ required: true, pattern: /\S+@\S+\.\S+/ })}
           />
-          <ErrorField
-            error={errors['email']}
+          <ErrorText error={errors['email']}
             messages={{
               required: 'Email is required',
               pattern: 'Invalid email address',
@@ -84,16 +54,10 @@ const Form = (props) => {
         </div>
         <div>
           <label>Mobile Number</label>
-
-          <input
-            type="tel"
-            placeholder="Mobile number"
-            name="mobileNumber"
+          <input type="tel" placeholder="Mobile number" name="mobileNumber"
             ref={register({ required: true, minLength: 6, maxLength: 12 })}
           />
-
-          <ErrorField
-            error={errors['mobileNumber']}
+          <ErrorText error={errors['mobileNumber']}
             messages={{
               required: 'Mobile number is required',
               minLength: 'Invalid mobile number',
@@ -112,43 +76,30 @@ const Form = (props) => {
         </div>
         <div>
           <label>Is developer?</label>
-          <input
-            name="isDeveloper"
-            type="radio"
-            value="Yes"
+          <input name="isDeveloper" type="radio" value="Yes"
             ref={register({ required: true })}
           />
-          <input
-            name="isDeveloper"
-            type="radio"
-            value="No"
+          <input name="isDeveloper" type="radio" value="No"
             ref={register({ required: true })}
           />
-          <ErrorField
-            error={errors['isDeveloper']}
+          <ErrorText error={errors['isDeveloper']}
             messages={{
               required: 'Please choose',
             }}
           />
         </div>
-
         <div>
           <div>
             <label>Tell something about yourself</label>
           </div>
-          <textarea
-            name="about"
-            ref={register({ required: true, minLength: 50 })}
-          ></textarea>
-          <ErrorField
-            error={errors['about']}
+          <textarea name="about" ref={register({ required: true, minLength: 50 })}></textarea>
+          <ErrorText error={errors['about']}
             messages={{
               required: 'Please tell something about yourself',
               minLength: 'Atleast 100 characters',
             }}
           />
         </div>
-
         <div>
           <label>Birthdate:</label>
           <Controller
@@ -172,15 +123,13 @@ const Form = (props) => {
               required: true,
             }}
           />
-          <ErrorField
-            error={errors['birthdate']}
+          <ErrorText error={errors['birthdate']}
             messages={{
               required: 'Please specify your birthdate',
               minDate: 'Date must not be in the future',
             }}
           />
         </div>
-
         <div>
           <label>Skills</label>
           <ul>
@@ -201,26 +150,21 @@ const Form = (props) => {
             </button>
           </section>
         </div>
-
         <div>
           <div>
-            <input
-              type={'checkbox'}
-              name="accept"
+            <input type={'checkbox'} name="accept"
               ref={register({
                 required: true,
               })}
             />
             <label>Accept Terms and condition</label>
           </div>
-          <ErrorField
-            error={errors['accept']}
+          <ErrorText error={errors['accept']}
             messages={{
               required: 'Please accept terms and condition',
             }}
           />
         </div>
-
         <input type="submit" />
       </form>
 
--- src/shared/components/form/errorText/errorText.component.js
+++ src/shared/components/form/errorText/errorText.component.js
@@ -2,9 +2,21 @@ import React from 'react'
 import style from './errorText.module.scss'
 
 const ErrorText = (props) => {
+  if (!props.error) {
+    return null
+  }
+
+  const errorMessages = {
+    required: 'This field is required',
+    maxLength: 'Max length reached',
+    minLength: 'Min length required',
+    pattern: 'Invalid pattern',
+    ...props.messages,
+  }
+
   return (
     <label className={`${props.className} ${style.errorText}`}>
-      {props.children}
+      {errorMessages[props.error.type] || 'Error'}
     </label>
   )
 }
